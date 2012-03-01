b315e9279a49fc60c24b08a779f315497535cfb7
Timur Abishev <abishev.timur@gmail.com>
2012-03-01 19:47:05 +0400

    Updated GraphCreator

--- src/ru/abishev/wiki/preprocessing/GraphCreator.java
+++ src/ru/abishev/wiki/preprocessing/GraphCreator.java
@@ -12,15 +12,15 @@ import java.nio.charset.Charset;
 import java.util.*;
 
 public class GraphCreator {
-    public static void createGraph(File subcatsInfoCsv, File ignoreCatsCsv, PrintWriter out) throws FileNotFoundException {
-        Set<String> ignoredCats = new HashSet<String>();
-        for (FileUtils.Chunk chunk : FileUtils.readFileAsChunks(ignoreCatsCsv, Charset.forName("UTF-8"), 100000)) {
-            if (chunk.isContinue) {
-                throw new RuntimeException(chunk.data);
-            }
-            ignoredCats.add(chunk.data.trim());
-        }
-        System.out.println("Ignoring cats: " + ignoredCats);
+    public static void createGraph(File subcatsInfoCsv, PrintWriter out) throws FileNotFoundException {
+//        Set<String> ignoredCats = new HashSet<String>();
+//        for (FileUtils.Chunk chunk : FileUtils.readFileAsChunks(ignoreCatsCsv, Charset.forName("UTF-8"), 100000)) {
+//            if (chunk.isContinue) {
+//                throw new RuntimeException(chunk.data);
+//            }
+//            ignoredCats.add(chunk.data.trim());
+//        }
+//        System.out.println("Ignoring cats: " + ignoredCats);
 
         out.println("cat_id|subcat_id");
         Map<Long, List<Long>> catToSubcats = new TreeMap<Long, List<Long>>();
@@ -33,77 +33,70 @@ public class GraphCreator {
                 long subCategoryId = subCategory.id;
 
                 if (Categories.INSTANCE.getById(subCategoryId) != null && Categories.INSTANCE.getById(categoryId) != null) {
-                    if (!ignoredCats.contains(Categories.INSTANCE.getById(subCategoryId).name) &&
-                            !ignoredCats.contains(Categories.INSTANCE.getById(categoryId).name)) {
-                        if (!catToSubcats.containsKey(categoryId)) {
-                            catToSubcats.put(categoryId, new ArrayList<Long>());
-                        }
-                        if (!subcatToCats.containsKey(subCategoryId)) {
-                            subcatToCats.put(subCategoryId, new ArrayList<Long>());
-                        }
-                        catToSubcats.get(categoryId).add(subCategoryId);
-                        subcatToCats.get(subCategoryId).add(categoryId);
+                    if (!catToSubcats.containsKey(categoryId)) {
+                        catToSubcats.put(categoryId, new ArrayList<Long>());
                     }
+                    if (!subcatToCats.containsKey(subCategoryId)) {
+                        subcatToCats.put(subCategoryId, new ArrayList<Long>());
+                    }
+                    catToSubcats.get(categoryId).add(subCategoryId);
+                    subcatToCats.get(subCategoryId).add(categoryId);
                 }
             }
         }
-
-        // remove all hidden categories
-        Category hidden = Categories.INSTANCE.getByName("Hidden_categories");
-        Set<Long> toRemove = new HashSet<Long>();
-        toRemove.add(hidden.id);
-        toRemove.addAll(catToSubcats.get(hidden.id));
-
-        // remove all redirect-class articles
-        Category redirectClass = Categories.INSTANCE.getByName("Redirect-Class_articles");
-        toRemove.add(redirectClass.id);
-        toRemove.addAll(catToSubcats.get(redirectClass.id));
-
-        // remove all categories with sockpuppets word
-        for (Category category : Categories.INSTANCE) {
-            if (category.name.toLowerCase().contains("sockpuppet") ||
-                    category.name.toLowerCase().endsWith("_templates") ||
-                    category.name.toLowerCase().startsWith("WikiProject_")) {
-                toRemove.add(category.id);
-            }
-        }
-
-        // find everything that have in superCats just toRemove cats
-        boolean isChanged = true;
-        while (isChanged) {
-            System.out.println("To remove: " + toRemove.size());
-
-            isChanged = false;
-            for (Category category : Categories.INSTANCE) {
-                if (subcatToCats.containsKey(category.id)) {
-                    if (!toRemove.contains(category.id)) {
-                        boolean isToRemove = true;
-                        for (long id : subcatToCats.get(category.id)) {
-                            if (!toRemove.contains(id)) {
-                                isToRemove = false;
-                                break;
-                            }
-                        }
-                        if (isToRemove) {
-                            isChanged = true;
-                            toRemove.add(category.id);
-//                            System.out.println(category);
-                        }
-                    }
-                }
-            }
-        }
-
-        System.out.println("To remove: " + toRemove.size());
+//
+//        // remove all hidden categories
+//        Category hidden = Categories.INSTANCE.getByName("Hidden_categories");
+//        Set<Long> toRemove = new HashSet<Long>();
+//        toRemove.add(hidden.id);
+//        toRemove.addAll(catToSubcats.get(hidden.id));
+//
+//        // remove all redirect-class articles
+//        Category redirectClass = Categories.INSTANCE.getByName("Redirect-Class_articles");
+//        toRemove.add(redirectClass.id);
+//        toRemove.addAll(catToSubcats.get(redirectClass.id));
+//
+//        // remove all categories with sockpuppets word
+//        for (Category category : Categories.INSTANCE) {
+//            if (category.name.toLowerCase().contains("sockpuppet") ||
+//                    category.name.toLowerCase().endsWith("_templates") ||
+//                    category.name.toLowerCase().startsWith("WikiProject_")) {
+//                toRemove.add(category.id);
+//            }
+//        }
+//
+//        // find everything that have in superCats just toRemove cats
+//        boolean isChanged = true;
+//        while (isChanged) {
+//            System.out.println("To remove: " + toRemove.size());
+//
+//            isChanged = false;
+//            for (Category category : Categories.INSTANCE) {
+//                if (subcatToCats.containsKey(category.id)) {
+//                    if (!toRemove.contains(category.id)) {
+//                        boolean isToRemove = true;
+//                        for (long id : subcatToCats.get(category.id)) {
+//                            if (!toRemove.contains(id)) {
+//                                isToRemove = false;
+//                                break;
+//                            }
+//                        }
+//                        if (isToRemove) {
+//                            isChanged = true;
+//                            toRemove.add(category.id);
+////                            System.out.println(category);
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        System.out.println("To remove: " + toRemove.size());
 
         for (long categoryId : catToSubcats.keySet()) {
-            if (!toRemove.contains(categoryId)) {
-                Collections.sort(catToSubcats.get(categoryId));
-                for (long subCatId : catToSubcats.get(categoryId)) {
-                    if (!toRemove.contains(subCatId)) {
-                        out.println(categoryId + "|" + subCatId);
-                    }
-                }
+            Collections.sort(catToSubcats.get(categoryId));
+            for (long subCatId : catToSubcats.get(categoryId)) {
+                out.println(categoryId + "|" + subCatId);
             }
         }
 
@@ -112,7 +105,6 @@ public class GraphCreator {
 
     public static void main(String[] args) throws FileNotFoundException {
         createGraph(new File("./data/enwiki-latest-categorylinks-subcats.csv"),
-                new File("./data/ignore_cats.txt"),
                 new PrintWriter(new File("./data/enwiki-catgraph.csv")));
     }
 }
