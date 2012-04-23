ebf110408aac79725cd019f0bdad757ac37aa455
Timur Abishev <abishev.timur@gmail.com>
2012-04-24 02:47:14 +0400

    programming detector is updated

--- src/ru/abishev/wiki/example2/ProgrammingDetector.java
+++ src/ru/abishev/wiki/example2/ProgrammingDetector.java
@@ -1,78 +1,63 @@
-//package ru.abishev.wiki.example2.example1;
-//
-//import ru.abishev.utils.CsvUtils;
-//import twitter4j.*;
-//import twitter4j.conf.ConfigurationBuilder;
-//
-//import java.io.File;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//public class ProgrammingDetector {
-//    public Map<String, Integer> stat = new HashMap<String, Integer>();
-//
-//    public ProgrammingDetector(File anchorsFile) {
-//        for (List<String> data : CsvUtils.readCsv(anchorsFile, '|', '"')) {
-//            for (String _w : splitOnPunctuation(data.get(0))) {
-//                String w = _w.toLowerCase();
-//                if (!stat.containsKey(w)) {
-//                    stat.put(w, 1);
-//                } else {
-//                    stat.put(w, stat.get(w) + 1);
-//                }
-//            }
-//        }
-//        System.out.println("Words loaded: " + stat.size());
-//    }
-//
-//    public int rate(String s) {
-//        int count = 0;
-//        for (String w : splitOnPunctuation(s)) {
-//            if (w.length() > 3 && stat.containsKey(w.toLowerCase())) {
-//                count += stat.get(w.toLowerCase());
-//            }
-//        }
-//        return count;
-//    }
-//
-//    private String[] splitOnPunctuation(String w) {
-//        return w.split("[ ,.!?:\"]");
-//    }
-//
-//    public static void main(String[] args) {
-//        final ProgrammingDetector detector = new ProgrammingDetector(new File("./data/fprog_anchors.csv"));
-//
-//        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-//        StatusListener listener = new StatusListener() {
-//            public void onStatus(Status status) {
-//                if (detector.rate(status.getText()) >= 1000) {
-//                    System.out.println(detector.rate(status.getText()) + "@" + status.getUser().getScreenName() + " - " + status.getText());
-//                }
-//            }
-//
-//            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-////                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-//            }
-//
-//            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-//                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-//            }
-//
-//            public void onScrubGeo(long userId, long upToStatusId) {
-//                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-//            }
-//
-//            public void onException(Exception ex) {
-//                ex.printStackTrace();
-//            }
-//        };
-//        twitterStream.addListener(listener);
-//        twitterStream.sample();
-//
-//        System.out.println(detector.rate("Clippy for Java Users: \"It appears you are trying to download the entire internet. Would you like help building your Java Project?\""));
-//        System.out.println(detector.rate("No our language is not OO even though its based on #Ruby. That's like saying a language must be imperative if it is implemented in C"));
-//    }
-//
-//
-//}
+package ru.abishev.wiki.example2;
+
+import ru.abishev.utils.CsvUtils;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProgrammingDetector {
+    public Map<String, Integer> progStat = new HashMap<String, Integer>();
+    public Map<String, Integer> allStat = new HashMap<String, Integer>();
+
+    public ProgrammingDetector(File progAnchorsFile, File allAnchorsStatFile) {
+        for (List<String> data : CsvUtils.readCsv(progAnchorsFile, '|', '"')) {
+            for (String w : splitOnPunctuation(data.get(0))) {
+                addToValue(progStat, w.toLowerCase(), 1);
+            }
+        }
+        System.out.println("Words loaded (prog): " + progStat.size());
+
+        for (List<String> data : CsvUtils.readCsv(allAnchorsStatFile, '|', '"')) {
+            int count = Integer.parseInt(data.get(2));
+            for (String w : splitOnPunctuation(data.get(0))) {
+                addToValue(allStat, w.toLowerCase(), count);
+            }
+        }
+        System.out.println("Words loaded (all): " + allStat.size());
+    }
+
+    private static <K> void addToValue(Map<K, Integer> map, K key, int addition) {
+        if (map.containsKey(key)) {
+            map.put(key, map.get(key) + addition);
+        } else {
+            map.put(key, addition);
+        }
+    }
+
+    public double rate(String s) {
+        double count = 0;
+        for (String w : splitOnPunctuation(s)) {
+            if (w.length() > 3 && progStat.containsKey(w.toLowerCase())) {
+                if (!allStat.containsKey(w.toLowerCase())) {
+                    System.out.println("oO " + w);
+                } else {
+                    count += progStat.get(w.toLowerCase()) * 1.0 / allStat.get(w.toLowerCase());
+                }
+            }
+        }
+        return count;
+    }
+
+    private String[] splitOnPunctuation(String w) {
+        return w.split("[ ,.!?:\"]");
+    }
+
+    public static void main(String[] args) {
+        final ProgrammingDetector detector = new ProgrammingDetector(new File("./data/prog_anchors.csv"), new File("./data/stat_5.txt"));
+
+        System.out.println(detector.rate("Clippy for Java Users: \"It appears you are trying to download the entire internet. Would you like help building your Java Project?\""));
+        System.out.println(detector.rate("No our language is not OO even though its based on #Ruby. That's like saying a language must be imperative if it is implemented in C"));
+    }
+}
---
+++ src/ru/abishev/wiki/example2/TwitterProgrammingDetector.java
@@ -0,0 +1,39 @@
+package ru.abishev.wiki.example2;
+
+import twitter4j.*;
+
+import java.io.File;
+
+public class TwitterProgrammingDetector {
+    public static void main(String[] args) {
+        final ProgrammingDetector detector = new ProgrammingDetector(new File("./data/prog_anchors.csv"), new File("./data/stat_5.txt"));
+
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        StatusListener listener = new StatusListener() {
+            public void onStatus(Status status) {
+                if (detector.rate(status.getText()) >= 0.2) {
+                    System.out.println(detector.rate(status.getText()) + "@" + status.getUser().getScreenName() + " - " + status.getText());
+                }
+            }
+
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+//                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            }
+
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+            }
+
+            public void onScrubGeo(long userId, long upToStatusId) {
+                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
+
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+        };
+        twitterStream.addListener(listener);
+        twitterStream.sample();
+    }
+
+}
