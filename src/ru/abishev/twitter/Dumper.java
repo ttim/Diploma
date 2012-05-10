52d82dcd70f994110c8c9f177a5f90e6dfb721ba
Timur Abishev <abishev.timur@gmail.com>
2012-05-10 22:48:55 +0400

    Added twitter users stream dumper and train set

---
+++ src/ru/abishev/twitter/Dumper.java
@@ -0,0 +1,21 @@
+package ru.abishev.twitter;
+
+import twitter4j.*;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Dumper {
+    public static void dump(String screenName) throws TwitterException, FileNotFoundException {
+        PrintWriter output = new PrintWriter("./train/" + screenName + ".source.txt");
+        Twitter twitter = new TwitterFactory().getInstance();
+        for (Status status : twitter.getUserTimeline(screenName, new Paging(1, 300))) {
+            output.println(status.getText().replaceAll("\n", " "));
+        }
+        output.close();
+    }
+
+    public static void main(String[] args) throws TwitterException, FileNotFoundException {
+        dump("NeilRobbins");
+    }
+}
