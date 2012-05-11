e9b98269fcaa8bdc0e1997f9cf9609cf69efd1c0
Timur Abishev <abishev.timur@gmail.com>
2012-05-12 00:15:44 +0400

    added new datasets

--- src/ru/abishev/twitter/Dumper.java
+++ src/ru/abishev/twitter/Dumper.java
@@ -7,7 +7,7 @@ import java.io.PrintWriter;
 
 public class Dumper {
     public static void dump(String screenName) throws TwitterException, FileNotFoundException {
-        PrintWriter output = new PrintWriter("./train/" + screenName + ".source.txt");
+        PrintWriter output = new PrintWriter("./train/_" + screenName + ".source.txt");
         Twitter twitter = new TwitterFactory().getInstance();
         for (Status status : twitter.getUserTimeline(screenName, new Paging(1, 300))) {
             output.println(status.getText().replaceAll("\n", " ")+"\t"+status.getId());
@@ -16,6 +16,10 @@ public class Dumper {
     }
 
     public static void main(String[] args) throws TwitterException, FileNotFoundException {
-        dump("NeilRobbins");
+//        dump("NeilRobbins");
+        dump("wycats");
+        dump("DHH");
+        dump("ladygaga");
+        dump("PerezHilton");
     }
 }
